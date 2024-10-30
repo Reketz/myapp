660c816f5133a5c03d35db8fc039ad4bd76a2951
Guilherme Estevao <guilherme.estevao@sankhya.com.br>
2024-10-30 19:36:24 -0300

    feat: (State) - atualizando objetos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import { useState } from 'react';
 import './App.css';
+import Box from './components/Button';
+
+const photo = {
+  name: 'Homenaje a la Neurocirugía',
+  artist: 'Marta Colvin Andrade',
+  description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'  
+}
 
 function App() {
 
-  const [count, setCount] = useState(0);
-  
-  return <div className='Container'>
-    {count}
-    <button 
-      className='Button' 
-      onClick={() => setCount(count + 1)} 
-      >+</button>
-  </div>
+  const [input, setInput] = useState('');
+  const [photoState, setPhotoState] = useState(photo);
+
+  function handleUpdate() {
+    setPhotoState({...photoState, novo: input});
+  }
+
+  function handleChange(event) {
+    setInput(event.target.value);
+  }
+
+
+  return (
+    <>
+    <input type="text" name="text" value={input} onChange={handleChange} />
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <span>{photoState.name}</span>
+      <span>{photoState.artist}</span>
+      <span>{photoState.description}</span>
+      {photoState.novo && <span>{photoState.novo}</span>}
+    </div>
+    <button type="button" onClick={handleUpdate}>
+      atualizar descrição
+    </button>
+
+  </>
+  )
 }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,3 @@
+export default function Box({ count, setCount }) {
+  return null;
+}
